fb7fe26c9bafb5ec4495801f3f1b5bd615fb9c68
asiya1330 <asiya.batool987@gmail.com>
2023-01-30 23:36:46 +0500

    React and firebase setup

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { app } from '../../services/firebaseConf'
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import './index.css';
+
+const Login = () => {
+
+  const [loading, setLoading] = useState(false);
+
+  const handleSignInWithGoogle = async () => {
+
+    setLoading(true);
+    try {
+      const auth = getAuth(app);
+      const provider = await new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const { user } = result;
+
+    }
+    catch (error) {
+      console.error(error);
+    }
+    finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className='login-page'>
+      <div className="" onClick={handleSignInWithGoogle}>
+        {
+          loading
+            ?
+            'Loading...'
+            :
+            <>
+              <button className='loginBtn'>
+                <i className="fab fa-google"></i>
+                . Sign in with Google
+              </button>
+            </>
+        }
+      </div>
+    </div>
+  );
+};
+
+export default Login;
+
